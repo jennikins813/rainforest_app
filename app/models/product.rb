64cb6981a3f210a04799dbbd76516fdddc04d8c5
jennikins813 <jennikins813@yahoo.com>
2014-04-14 20:57:15 -0400

    Display price in dollars using the formatted_price method

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,14 @@
 class Product < ActiveRecord::Base
 	validates :description, :name, :presence => true
 	validates :price_in_cents, :numericality => {:only_integer => true}
-end
 
 	#when you create a product, both description and name have to be present.
 	#When you create a product, the price_in_cents must be an integer.
+
+	def formatted_price
+		price_in_dollars = price_in_cents.to_f / 100
+		sprintf("%.2f", price_in_dollars)
+	end
+	#writing this method in the product model converts the price_in_cents to a string that is a price in dollars.
+
+end
